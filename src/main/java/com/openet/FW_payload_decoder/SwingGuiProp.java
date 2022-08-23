8781b4cb18595e5df2141dc54de393dd4d9d9a90
locbe2002 <82045138+locbe2002@users.noreply.github.com>
2022-08-23 10:24:46 -0400

    Update SwingGuiProp.java

--- src/main/java/com/openet/FW_payload_decoder/SwingGuiProp.java
+++ src/main/java/com/openet/FW_payload_decoder/SwingGuiProp.java
@@ -363,7 +363,7 @@ public class SwingGuiProp implements ActionListener, PropertyChangeListener {
 //			byte[] arrayOfByte1 = DatatypeConverter.parseHexBinary(PmPayloadHex);
 //        		System.out.println("data.length: " + PmPayloadHex.length() + ", compressed.length: " + arrayOfByte1.length);
 //        		byte[] arrayOfByte2 = Snappy.uncompress(arrayOfByte1);
-			String str2 = new String (Snappy.uncompress(hexToASCII(PmPayloadHex)).getBytes(), "UTF-8");
+			String str2 = new String (Snappy.uncompress(hexToASCII(PmPayloadHex).getBytes()), "UTF-8");
 //			String str2 = new String(arrayOfByte2, "UTF-8");
 //			System.out.println("you are before return");
         		return str2;
