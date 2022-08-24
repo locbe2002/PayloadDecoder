959675bb2e263de6ca39615355498ff59196e6a8
locbe2002 <82045138+locbe2002@users.noreply.github.com>
2022-08-24 13:29:28 -0400

    Update JsonSnappyDecoder.java

--- src/main/java/com/openet/JsonSnappyDecoder/JsonSnappyDecoder.java
+++ src/main/java/com/openet/JsonSnappyDecoder/JsonSnappyDecoder.java
@@ -13,10 +13,10 @@ public class JsonSnappyDecoder {
       java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.FileReader(paramArrayOfString[0]));
       line = reader.readLine();
       while (line != null) {
-        byte[] arrayOfByte1 = DatatypeConverter.parseHexBinary(line);
+//        byte[] arrayOfByte1 = DatatypeConverter.parseHexBinary(line);
 //        System.out.println("data.length: " + line.length() + ", compressed.length: " + arrayOfByte1.length);
-        byte[] arrayOfByte2 = Snappy.uncompress(arrayOfByte1);
-        String OriJson = new String(arrayOfByte2, "UTF-8");
+//        byte[] arrayOfByte2 = Snappy.uncompress(arrayOfByte1);
+//        String OriJson = new String(arrayOfByte2, "UTF-8");
         String OriJson = new String (Snappy.uncompress(DatatypeConverter.parseHexBinary(line)), "UTF-8");
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         JsonElement jsonElement = JsonParser.parseString(OriJson);
